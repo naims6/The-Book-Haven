a7ee29770b2d4b2467837afe603736833ff24171
naims6 <naimdrive6@gmail.com>
2025-11-10 00:16:45 +0600

    install react toastify

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,23 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./Routes/Routes.jsx";
 import AuthProvider from "./Provider/AuthProvider.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </AuthProvider>
   </StrictMode>
 );
