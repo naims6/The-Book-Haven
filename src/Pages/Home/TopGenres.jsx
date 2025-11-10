8fdc541ccef78b426f1b243aba3b77441ca50c54
naims6 <naimdrive6@gmail.com>
2025-11-10 17:21:27 +0600

    top genres section responsive fixed

--- src/Pages/Home/TopGenres.jsx
+++ src/Pages/Home/TopGenres.jsx
@@ -25,28 +25,30 @@ const genres = [
 
 const TopGenres = () => {
   return (
-    <section className="py-10 px-6 md:px-12 bg-white dark:bg-gray-900">
-      <h2 className="text-3xl font-bold  mb-8 text-center">Top Genres</h2>
+    <section className="py-10 md:px-10 bg-white dark:bg-gray-900">
+      <div className="container2">
+        <h2 className="text-3xl font-bold  mb-8 text-center">Top Genres</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {genres.map((genre, index) => (
-          <div
-            key={index}
-            className="relative rounded-2xl overflow-hidden shadow-lg group cursor-pointer"
-          >
-            <img
-              src={genre.img}
-              alt={genre.title}
-              className="w-full h-64 object-cover group-hover:scale-110 transition-transform duration-500"
-            />
-            <div className="absolute inset-0 bg-black/40 group-hover:bg-black/50 transition-all duration-300"></div>
-            <div className="absolute bottom-4 left-4">
-              <span className="bg-white text-green-700 text-sm font-semibold px-3 py-3 rounded">
-                {genre.title}
-              </span>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {genres.map((genre, index) => (
+            <div
+              key={index}
+              className="relative rounded-2xl overflow-hidden shadow-lg group cursor-pointer"
+            >
+              <img
+                src={genre.img}
+                alt={genre.title}
+                className="w-full h-64 object-cover group-hover:scale-110 transition-transform duration-500"
+              />
+              <div className="absolute inset-0 bg-black/40 group-hover:bg-black/50 transition-all duration-300"></div>
+              <div className="absolute bottom-4 left-4">
+                <span className="bg-white text-green-700 text-sm font-semibold px-3 py-3 rounded">
+                  {genre.title}
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
