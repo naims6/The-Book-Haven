0de3b5e1630ddc6f714eb7b2d63d8a503ce693ef
naims6 <naimdrive6@gmail.com>
2025-11-10 15:40:26 +0600

    add: top genres section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import LatestBooks from "./LatestBooks";
+import TopGenres from "./TopGenres";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <LatestBooks />
+      <TopGenres />
     </div>
   );
 };
