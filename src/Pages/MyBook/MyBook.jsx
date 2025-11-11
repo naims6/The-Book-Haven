73d9cbf717e94f47307ab801e949ff6688635f2c
naims6 <naimdrive6@gmail.com>
2025-11-11 13:57:25 +0600

    add: my book page

--- src/Pages/MyBook/MyBook.jsx
+++ src/Pages/MyBook/MyBook.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+import Title2 from "../../Components/Title2";
+import useAxios from "../../hooks/useAxios";
+import BookCardSkeleton from "../AllBook/BookCardSkeleton";
+import BookCard from "../AllBook/BookCard";
+import { AuthContext } from "../../Provider/AuthContex";
 
 const MyBook = () => {
-  return <div>my book</div>;
+  const { user } = use(AuthContext);
+  const axiosInstance = useAxios();
+  const [books, setBooks] = useState();
+  const [loading, setLoading] = useState(true);
+
+  // Fetch data client-side
+  useEffect(() => {
+    setTimeout(() => {
+      axiosInstance
+        .get(`/all-books?email=${user?.email}`)
+        .then((data) => {
+          setBooks(data.data);
+          setLoading(false);
+        })
+        .catch(() => setLoading(false));
+    }, 500);
+  }, [axiosInstance, user]);
+
+  return (
+    <div className="container2 mt-16 py-12">
+      <Title2>My Book</Title2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 container2">
+        {loading
+          ? Array.from({ length: 4 }).map((_, i) => (
+              <BookCardSkeleton key={i} />
+            ))
+          : books.map((book) => <BookCard key={book._id} book={book} />)}
+      </div>
+    </div>
+  );
 };
 
 export default MyBook;
